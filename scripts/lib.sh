3d3b80875b81981f416bd3e577d8a5d94fa2f100
DJ Adams <dj.adams@sap.com>
2023-01-04 12:47:07 +0000

    deal with differences between cf 7 and 8 JSON output fixes #27

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -2,6 +2,8 @@
 # shellcheck disable=SC2034
 
 # To be sourced within the generate-* scripts in this directory.
+# We need to deal with cf version 7 and 8 JSON differences in service
+# key data; the latter starts with a single `credentials` property.
 
 set -o errexit
 
@@ -10,9 +12,9 @@ declare uaa_url clientid clientsecret service_key_file
 service_key_file="${1:?Specify name of file containing service key data}"
 shift
 
-uaa_url="$(jq -r .uaa.url "${service_key_file}")"
-clientid="$(jq -r .uaa.clientid "${service_key_file}")"
-clientsecret="$(jq -r .uaa.clientsecret "${service_key_file}")"
+uaa_url="$(jq -r '(.credentials//.).uaa.url' "${service_key_file}")"
+clientid="$(jq -r '(.credentials//.).uaa.clientid' "${service_key_file}")"
+clientsecret="$(jq -r '(.credentials//.).uaa.clientsecret' "${service_key_file}")"
 
 if [[ -z $uaa_url ]]; then
   echo "No service key data available"
